a6b6c01488d9ad147dc7e4198f95c23345fac915
pancsidd <50750003+pancsidd@users.noreply.github.com>
2020-07-27 07:56:51 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,8 @@
+import { books } from 'src/app/bookInfo';
+
+export const mybooks: books[] = [
+  { bname: 'Intro to Linux', bauthor: 'Harry Potter', bgenre: 'Education', bpicture: 'assets/images/linux.jpg', byear: '2007' },
+  { bname: 'Intro to Java', bauthor: 'James Potter', bgenre: 'Sci-Fi', bpicture: 'assets/images/java.jpg', byear: '2002' },
+  { bname: 'Intro to Database', bauthor: 'Lily Potter', bgenre: 'Comedy', bpicture: 'assets/images/database.jpg', byear: '2009' },
+  { bname: 'Into to .NET', bauthor: 'Lord Voldemort', bgenre: 'Drama', bpicture: 'assets/images/net.jpg', byear: '2012' }
+]
